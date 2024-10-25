148c96988a54b1f5068037a5db35d270e83ed762
allisontimossi <allison.timossi@hotmail.it>
2024-10-25 09:57:41 +0200

    product model added

---
+++ Models/Product.cs
@@ -0,0 +1,8 @@
+public class Product
+{
+    public int Id {get;set;}
+    public string Name {get;set;}   
+    public float Price {get;set;}    
+    public int Amount {get;set;}  
+    public int CategoryId {get;set;}    
+}
