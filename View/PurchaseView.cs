65ecb65ba9b4f6b86ca98b3a78db29e4464dd6d1
mtm90 <mattia22nchi@gmail.com>
2024-11-04 11:51:46 +0100

    modified purchase view

--- View/PurchaseView.cs
+++ View/PurchaseView.cs
@@ -7,7 +7,7 @@ public class PurchaseView
         Console.WriteLine("1 - Show Purchases");       // Option to display all purchases
         Console.WriteLine("2 - Add Purchase");         // Option to add a new purchase
         Console.WriteLine("3 - Delete Purchase");         // Option to add a new purchase
-        Console.WriteLine("3 - Back");                 // Option to return to the previous menu
+        Console.WriteLine("4 - Back");                 // Option to return to the previous menu
         Console.WriteLine("Make your selection");      // Prompt for user input
     }
 
